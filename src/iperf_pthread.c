f2507e97a257ed3fcdf743d7d3d2b63daac0108e
Johann <9853102+hajoha@users.noreply.github.com>
2025-04-28 19:46:41 +0200

    Update to v3.18 (#3)
    * update README
    * bump to 3.18

--- src/iperf_pthread.c
+++ src/iperf_pthread.c
@@ -7,28 +7,9 @@
  * as Android NDK does not support `pthread_cancel()`.
  */
 
-#include <string.h>
 #include <signal.h>
 #include <string.h>
 #include "iperf_pthread.h"
-
-void iperf_thread_exit_handler(int sig)
-{
-    pthread_exit(0);
-}
-
-int iperf_set_thread_exit_handler() {
-    int rc;
-    struct sigaction actions;
-
-    memset(&actions, 0, sizeof(actions));
-    sigemptyset(&actions.sa_mask);
-    actions.sa_flags = 0;
-    actions.sa_handler = iperf_thread_exit_handler;
-
-    rc = sigaction(SIGUSR1, &actions, NULL);
-    return rc;
-}
 
 int pthread_setcanceltype(int type, int *oldtype) { return 0; }
 int pthread_setcancelstate(int state, int *oldstate) { return 0; }
